16dbcda8b7927d06e324076a4dd49db3e001af0b
Rob Kuhlig <rob@bpmspace.org>
2017-04-25 14:19:52 +0200

    new table exam version

--- mysql_dump/20170309_dump_sqms_diff_minimumdata_after_v6.sql
+++ mysql_dump/20170309_dump_sqms_diff_minimumdata_after_v6.sql
@@ -13,3 +13,27 @@ INSERT INTO `bpmspace_sqms_v6`.`sqms_question_type` (`sqms_question_type_id`, `n
 INSERT INTO `bpmspace_sqms_v6`.`sqms_question_type` (`sqms_question_type_id`, `name`) VALUES ('11', 'Random short-answer matching');
 INSERT INTO `bpmspace_sqms_v6`.`sqms_question_type` (`sqms_question_type_id`, `name`) VALUES ('12', 'True/False');
 INSERT INTO `bpmspace_sqms_v6`.`sqms_question_type` (`sqms_question_type_id`, `name`) VALUES ('4', 'Description');
+
+
+CREATE TABLE `sqms_exam_version` (
+  `sqms_exam_version_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sqms_exam_version_name` longtext,
+  `sqms_question_unique_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`sqms_exam_version_id`) 
+) ENGINE=InnoDB AUTO_INCREMENT=678 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `sqms_question_exam_version` (
+  `sqms_question_exam_version_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sqms_exam_version_id` int(11),
+  `sqms_question_id` int(11),
+  PRIMARY KEY (`sqms_question_exam_version_id`) 
+) ENGINE=InnoDB AUTO_INCREMENT=287 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `bpmspace_sqms_v6`.`sqms_question_exam_version` 
+ADD INDEX `sqms_exam_version_id_fk_1_idx` (`sqms_exam_version_id` ASC);
+ALTER TABLE `bpmspace_sqms_v6`.`sqms_question_exam_version` 
+ADD CONSTRAINT `sqms_exam_version_id_fk_1`
+  FOREIGN KEY (`sqms_exam_version_id`)
+  REFERENCES `bpmspace_sqms_v6`.`sqms_exam_version` (`sqms_exam_version_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
