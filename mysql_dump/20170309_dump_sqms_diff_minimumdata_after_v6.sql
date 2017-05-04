fcb3876e8e482a50ded83e0e94aa3f31460f38c6
Idefixus <robinwuerz@web.de>
2017-05-04 11:49:20 +0200

    Diff data update for tables for question sets and their constraints

--- mysql_dump/20170309_dump_sqms_diff_minimumdata_after_v6.sql
+++ mysql_dump/20170309_dump_sqms_diff_minimumdata_after_v6.sql
@@ -14,35 +14,39 @@ INSERT INTO `bpmspace_sqms_v6`.`sqms_question_type` (`sqms_question_type_id`, `n
 INSERT INTO `bpmspace_sqms_v6`.`sqms_question_type` (`sqms_question_type_id`, `name`) VALUES ('12', 'True/False');
 INSERT INTO `bpmspace_sqms_v6`.`sqms_question_type` (`sqms_question_type_id`, `name`) VALUES ('4', 'Description');
 
+# Create table for different question sets.
+DROP TABLE IF EXISTS `sqms_exam_version`;
 
 CREATE TABLE `sqms_exam_version` (
   `sqms_exam_version_id` int(11) NOT NULL AUTO_INCREMENT,
   `sqms_exam_version_name` longtext,
-  `sqms_question_unique_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`sqms_exam_version_id`) 
+  PRIMARY KEY (`sqms_exam_version_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=678 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `sqms_question_exam_version` (
-  `sqms_question_exam_version_id` int(11) NOT NULL AUTO_INCREMENT,
-  `sqms_exam_version_id` int(11),
-  `sqms_question_id` int(11),
-  PRIMARY KEY (`sqms_question_exam_version_id`) 
-) ENGINE=InnoDB AUTO_INCREMENT=287 DEFAULT CHARSET=utf8;
+# Create table with questions and answers and their coresponding set in sqms_exam_version
 
-ALTER TABLE `bpmspace_sqms_v6`.`sqms_question_exam_version` 
-ADD INDEX `sqms_exam_version_id_fk_1_idx` (`sqms_exam_version_id` ASC);
-ALTER TABLE `bpmspace_sqms_v6`.`sqms_question_exam_version` 
-ADD CONSTRAINT `sqms_exam_version_id_fk_1`
-  FOREIGN KEY (`sqms_exam_version_id`)
-  REFERENCES `bpmspace_sqms_v6`.`sqms_exam_version` (`sqms_exam_version_id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
+DROP TABLE IF EXISTS `sqms_question_answer_exam_version`;
 
-ADD CONSTRAINT `sqms_question_id_fk_2` 
-  FOREIGN KEY (`sqms_question_id`) 
-  REFERENCES `sqms_question` (`sqms_question_id`) 
-  ON DELETE NO ACTION 
-  ON UPDATE NO ACTION;
+CREATE TABLE `sqms_question_answer_exam_version` (
+  `sqms_question_answer_exam_version_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sqms_exam_version_id` int(11) NOT NULL,
+  `sqms_question_id` bigint(20) NOT NULL,
+  `sqms_answer_id_1` bigint(20) DEFAULT NULL,
+  `sqms_answer_id_2` bigint(20) DEFAULT NULL,
+  `sqms_answer_id_3` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`sqms_question_answer_exam_version_id`),
+  KEY `sqms_exam_version_id_fk_1_idx` (`sqms_exam_version_id`),
+  KEY `test_idx` (`sqms_question_id`),
+  KEY `sqms_answer_id_1_fk_3_idx` (`sqms_answer_id_1`),
+  KEY `sqms_answer_id_2_fk_4_idx` (`sqms_answer_id_2`),
+  KEY `sqms_answer_id_3_fk_5_idx` (`sqms_answer_id_3`),
+  CONSTRAINT `sqms_exam_version_id_fk_01` FOREIGN KEY (`sqms_exam_version_id`) REFERENCES `sqms_exam_version` (`sqms_exam_version_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sqms_answer_id_1_fk_3` FOREIGN KEY (`sqms_answer_id_1`) REFERENCES `sqms_answer` (`sqms_answer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sqms_answer_id_2_fk_4` FOREIGN KEY (`sqms_answer_id_2`) REFERENCES `sqms_answer` (`sqms_answer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sqms_answer_id_3_fk_5` FOREIGN KEY (`sqms_answer_id_3`) REFERENCES `sqms_answer` (`sqms_answer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sqms_question_id_fk_02` FOREIGN KEY (`sqms_question_id`) REFERENCES `sqms_question` (`sqms_question_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
 
 # Add triggers
 DROP TRIGGER IF EXISTS `bpmspace_sqms_v6.question_after_update`;
@@ -75,39 +79,39 @@ CREATE
     //
     DELIMITER ;
 
-# Create a new table as copy of coms_participant.
+# Create a new table as copy of coms_participant. TODO: Alter to new generic logging
 
-DROP TABLE IF EXISTS bpmspace_coms_v1.coms_participant_history;
+-- DROP TABLE IF EXISTS bpmspace_coms_v1.coms_participant_history;
 
-CREATE TABLE `coms_participant_history` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `coms_participant_id` int(11) NOT NULL,
-  `coms_participant_lastname` varchar(60) DEFAULT NULL,
-  `coms_participant_firstname` varchar(60) DEFAULT NULL,
-  `coms_participant_public` tinyint(4) DEFAULT '0',
-  `coms_participant_placeofbirth` varchar(60) DEFAULT NULL,
-  `coms_participant_birthcountry` varchar(60) DEFAULT NULL,
-  `coms_participant_dateofbirth` date DEFAULT NULL,
-  `coms_participant_LIAM_id` int(11) NOT NULL,
-  `coms_participant_gender` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+-- CREATE TABLE `coms_participant_history` (
+--   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+--   `coms_participant_id` int(11) NOT NULL,
+--   `coms_participant_lastname` varchar(60) DEFAULT NULL,
+--   `coms_participant_firstname` varchar(60) DEFAULT NULL,
+--   `coms_participant_public` tinyint(4) DEFAULT '0',
+--   `coms_participant_placeofbirth` varchar(60) DEFAULT NULL,
+--   `coms_participant_birthcountry` varchar(60) DEFAULT NULL,
+--   `coms_participant_dateofbirth` date DEFAULT NULL,
+--   `coms_participant_LIAM_id` int(11) NOT NULL,
+--   `coms_participant_gender` varchar(10) DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
-# Create trigger for coms participants.
+# Create trigger for coms participants. TODO: Create trigger for inserting in generic table.
 
-DROP TRIGGER IF EXISTS `bpmspace_coms_v1.coms_participant_update`;
-DELIMITER //
+-- DROP TRIGGER IF EXISTS `bpmspace_coms_v1.coms_participant_update`;
+-- DELIMITER //
 
-CREATE
-	TRIGGER `bpmspace_coms_v1.coms_participant_update` AFTER UPDATE 
-	ON `coms_participant`
-	FOR EACH ROW BEGIN
+-- CREATE
+-- 	TRIGGER `bpmspace_coms_v1.coms_participant_update` AFTER UPDATE 
+-- 	ON `coms_participant`
+-- 	FOR EACH ROW BEGIN
 	
-	INSERT INTO coms_participant_history (coms_participant_id, coms_participant_lastname, coms_participant_firstname, coms_participant_public, coms_participant_placeofbirth, coms_participant_birthcountry, coms_participant_dateofbirth, coms_participant_LIAM_id, coms_participant_gender) 
-	VALUES (OLD.coms_participant_id, OLD.coms_participant_lastname, OLD.coms_participant_firstname, OLD.coms_participant_public, OLD.coms_participant_placeofbirth, OLD.coms_participant_birthcountry, OLD.coms_participant_dateofbirth, OLD.coms_participant_LIAM_id, OLD.coms_participant_gender);
+-- 	INSERT INTO coms_participant_history (coms_participant_id, coms_participant_lastname, coms_participant_firstname, coms_participant_public, coms_participant_placeofbirth, coms_participant_birthcountry, coms_participant_dateofbirth, coms_participant_LIAM_id, coms_participant_gender) 
+-- 	VALUES (OLD.coms_participant_id, OLD.coms_participant_lastname, OLD.coms_participant_firstname, OLD.coms_participant_public, OLD.coms_participant_placeofbirth, OLD.coms_participant_birthcountry, OLD.coms_participant_dateofbirth, OLD.coms_participant_LIAM_id, OLD.coms_participant_gender);
 
-    END
-    //
-    DELIMITER ;
+--     END
+--     //
+--     DELIMITER ;
 
