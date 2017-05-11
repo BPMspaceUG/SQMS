feeb66d193d7eed51ac06cca8ca1d68951e47938
Idefixus <robinwuerz@web.de>
2017-05-11 09:41:24 +0200

    Corrections to SQLDump diff structure and minimumdata, add structure for moodle export of sets

--- mysql_dump/20170309_dump_sqms_diff_structure_after_v6.sql
+++ mysql_dump/20170309_dump_sqms_diff_structure_after_v6.sql
@@ -100,3 +100,108 @@ create view v_sqms_syllabus_syllabuselement_question as
   LEFT JOIN sqms_question as QUEST ON SYLEL_Q.sqms_question_id = QUEST.sqms_question_id
   
   ORDER BY SYLLA.sqms_syllabus_id, SYLEL.element_order, QUEST.sqms_question_id  ASC;
+
+  DROP TABLE IF EXISTS `sqms_history`;
+
+CREATE TABLE `sqms_history` (
+  `sqms_history_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `sqms_users_login` varchar(32) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `table_name` varchar(45) NOT NULL,
+  `main_id` bigint(20) DEFAULT NULL,
+  `column_name` varchar(45) NOT NULL,
+  `value_OLD` varchar(1024) DEFAULT NULL,
+  `value_NEW` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`sqms_history_id`,`sqms_users_login`),
+  KEY `fk_sqms_history_sqms_users1_idx` (`sqms_users_login`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+
+# Create table for different question sets.
+DROP TABLE IF EXISTS `sqms_exam_version`;
+
+CREATE TABLE `sqms_exam_version` (
+  `sqms_exam_version_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sqms_exam_version_name` longtext,
+  PRIMARY KEY (`sqms_exam_version_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=678 DEFAULT CHARSET=utf8;
+
+# Create table with questions and answers and their coresponding set in sqms_exam_version
+
+DROP TABLE IF EXISTS `sqms_question_answer_exam_version`;
+
+CREATE TABLE `sqms_question_answer_exam_version` (
+  `sqms_question_answer_exam_version_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sqms_exam_version_id` int(11) NOT NULL,
+  `sqms_question_id` bigint(20) NOT NULL,
+  `sqms_answer_id_1` bigint(20) DEFAULT NULL,
+  `sqms_answer_id_2` bigint(20) DEFAULT NULL,
+  `sqms_answer_id_3` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`sqms_question_answer_exam_version_id`),
+  KEY `sqms_exam_version_id_fk_1_idx` (`sqms_exam_version_id`),
+  KEY `test_idx` (`sqms_question_id`),
+  KEY `sqms_answer_id_1_fk_3_idx` (`sqms_answer_id_1`),
+  KEY `sqms_answer_id_2_fk_4_idx` (`sqms_answer_id_2`),
+  KEY `sqms_answer_id_3_fk_5_idx` (`sqms_answer_id_3`),
+  CONSTRAINT `sqms_exam_version_id_fk_01` FOREIGN KEY (`sqms_exam_version_id`) REFERENCES `sqms_exam_version` (`sqms_exam_version_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sqms_answer_id_1_fk_3` FOREIGN KEY (`sqms_answer_id_1`) REFERENCES `sqms_answer` (`sqms_answer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sqms_answer_id_2_fk_4` FOREIGN KEY (`sqms_answer_id_2`) REFERENCES `sqms_answer` (`sqms_answer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sqms_answer_id_3_fk_5` FOREIGN KEY (`sqms_answer_id_3`) REFERENCES `sqms_answer` (`sqms_answer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sqms_question_id_fk_02` FOREIGN KEY (`sqms_question_id`) REFERENCES `sqms_question` (`sqms_question_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+
+# Add triggers
+DROP TRIGGER IF EXISTS `bpmspace_sqms_v6.question_after_update`;
+DELIMITER //
+
+CREATE
+  TRIGGER `bpmspace_sqms_v6.question_after_update` AFTER UPDATE 
+  ON `sqms_question`
+  FOR EACH ROW BEGIN
+  
+  INSERT INTO sqms_history (sqms_users_login, timestamp, table_name, main_id, column_name, value_OLD, value_NEW) 
+    VALUES (CURRENT_USER(), NOW(), "sqms_question", OLD.sqms_question_id, "sqms_question_state_id", OLD.sqms_question_state_id, NEW.sqms_question_state_id);
+  
+    END
+    //
+    DELIMITER ;
+  
+DROP TRIGGER IF EXISTS `bpmspace_sqms_v6.answer_after_update`;
+DELIMITER //
+
+CREATE
+  TRIGGER `bpmspace_sqms_v6.answer_after_update` AFTER UPDATE 
+  ON `sqms_answer`
+  FOR EACH ROW BEGIN
+  
+  INSERT INTO sqms_history (sqms_users_login, timestamp, table_name, main_id, column_name, value_OLD, value_NEW) 
+    VALUES (CURRENT_USER(), NOW(), "sqms_answer", OLD.sqms_answer_id, "sqms_question_id", OLD.sqms_question_id, NEW.sqms_question_id);
+  
+    END
+    //
+    DELIMITER ;
+
+Create or replace view v_sqms_short_xml_export_moodle as
+Select concat('[',lpad(d.sqms_question_id,4,'0'),'-',
+lpad(d.sqms_answer_id_1,4,'0'),'-',
+lpad(d.sqms_answer_id_2,4,'0'),'-',
+lpad(d.sqms_answer_id_3,4,'0'),']',
+f.name,'-',g.language,'-',substr(e.question ,1,20)) AS `name_text_preview`,
+e.question, 
+a.answer as answer_1, CASE 
+            WHEN a.correct 
+               THEN 100.00000
+               ELSE 0 
+       END as fraction_id_1, 
+b.answer as answer_2, CASE 
+            WHEN b.correct 
+               THEN 100.00000
+               ELSE 0 
+       END as fraction_id_2, 
+c.answer as answer_3, CASE 
+            WHEN c.correct 
+               THEN 100.00000
+               ELSE 0 
+       END as fraction_id_3
+FROM bpmspace_sqms_v6.sqms_question_answer_exam_version d natural join sqms_topic f natural join sqms_language g natural join sqms_question e join sqms_answer a on a.sqms_answer_id = d.sqms_answer_id_1 join sqms_answer b on b.sqms_answer_id = sqms_answer_id_2 join sqms_answer c on c.sqms_answer_id = sqms_answer_id_3;
